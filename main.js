ed28e5f6817dacf153516af9650aee4d848c1e1d
mohsin <mdmohsineng@gmail.com>
2022-07-03 01:20:59 +0600

    add audio with done my audio play project

---
+++ main.js
@@ -0,0 +1,24 @@
+ 
+
+for(var i=0; i<3; i++){
+    document.querySelectorAll(".myDiv")[i].addEventListener("click", function(){
+        var text = this.innerHTML; 
+        switch(text){
+            case "a":
+                var audio = new Audio("sound/a.mp3");
+                audio.play();
+                break;
+    
+                case "b":
+                    var audio = new Audio("sound/b.mp3");
+                    audio.play();
+                    break;
+    
+               case "c":
+                var audio = new Audio("sound/a.mp3");
+                audio.play();
+                break;
+        }
+    });
+   
+}
